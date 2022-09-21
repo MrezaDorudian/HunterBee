02bfef93d1f9416a844fe6d478aa186d90ac36aa
Mohammadreza <mr.dorudian@gmail.com>
2022-09-21 15:14:28 +0430

    save and add_to_server feature added

--- sysmon/sysmon.py
+++ sysmon/sysmon.py
@@ -1,5 +1,11 @@
+import os
 import subprocess
+import time
+import socket
+import threading
+import yaml
 
+import logs
 
 def check_installed():
     try:
@@ -23,12 +29,9 @@ def check_version():
 
 def get_config():
     if check_installed():
-        result = subprocess.run(['sysmon.exe', '-s'], capture_output=True, text=True)
-
-        config = f'<{"<".join(result.stdout.split("<")[1:])}'
+        result = subprocess.run(['sysmon.exe', '-c'], capture_output=True, text=True)
+        config = f'configuration{"".join(result.stdout.split("Rule configuration")[1:])}'
         print(config)
-        with open('current_config.xml', 'w') as f:
-            f.write(config)
 
 
 def set_config(file_address):
@@ -36,3 +39,43 @@ def set_config(file_address):
         subprocess.run(['sysmon.exe', '-c', file_address])
     else:
         print('Not installed')
+
+
+def save_logs():
+    if check_installed():
+        while True:
+            ndjson_folders = [x for x in os.listdir(logs.get_folder_address()) if x.count('.ndjson') > 0]
+            json_folders = [x for x in os.listdir(logs.get_folder_address()) if x.count('.json') > 0]
+
+            if len(ndjson_folders) > 1 or len(json_folders) > 1:
+                if len(ndjson_folders) > 1:
+                    file_id = ndjson_folders[0].split('.')[0].split('-')[-1]
+                    logs.read_logs(ndjson_folders[0], file_id)
+                    os.remove(f'{logs.get_folder_address()}/{ndjson_folders[0]}')
+
+                if len(json_folders) > 1:
+                    send_to_server(f'{logs.get_folder_address()}/{json_folders[0]}')
+                    os.remove(f'{logs.get_folder_address()}/{json_folders[0]}')
+            else:
+                time.sleep(10)
+    else:
+        print('Not installed')
+
+
+def send_to_server(file_address):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(get_server_info())
+    #     send log files to server
+    with open(file_address, 'rb') as f:
+        client.send(f.read())
+    os.remove(file_address)
+
+
+
+def get_server_info():
+    with open('../config.yaml') as file:
+        config = yaml.safe_load(file)
+        return config['server']['address'], config['server']['port']
+
+
+save_logs()
