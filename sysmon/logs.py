cdec8975a159e1facdffbad8b59e3c7af544962c
Mohammadreza <mr.dorudian@gmail.com>
2022-09-21 15:13:40 +0430

    read_logs function refactored

--- sysmon/logs.py
+++ sysmon/logs.py
@@ -1,20 +1,33 @@
 import os
 import yaml
 import ndjson
+import json
 
 
-def read_logs():
-    log_files = []
-    for file in os.listdir(get_folder_address()):
-        log_files.append(file)
-    for file in log_files:
-        with open(f'{get_folder_address()}/{file}') as f:
-            data = ndjson.load(f)
-            yield data
+def read_logs(file_name, counter):
+    output_data = {}
+    iterator = 0
+    with open(f'{get_folder_address()}/{file_name}') as f:
+        data = ndjson.load(f)
+        for item in data:
+            for key in ['@metadata', 'event', 'log', 'ecs', 'agent', 'message']:
+                item.pop(key)
+            for key in ['user', 'process', 'record_id', 'api', 'opcode', 'event_id', 'version', 'channel']:
+                item['winlog'].pop(key)
+            for key in ['os', 'mac', 'id', 'hostname', 'architecture']:
+                item['host'].pop(key)
+            output_data[iterator] = item
+            iterator += 1
+    # write output data as json file
+    # json_address
+    with open('../config.yaml') as file:
+        config = yaml.safe_load(file)
+        json_address = config['sysmon']['json_address']
+    with open(f'{json_address}/sysmon-logs-{counter}.json', 'w') as f:
+        json.dump(output_data, f, indent=4)
 
 
 def get_folder_address():
     with open('../config.yaml') as file:
         config = yaml.safe_load(file)
         return config['sysmon']['folder_address']
-
