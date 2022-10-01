97051eacf4e6ebd5bbfa46cb39cdf4111604dc4c
Mohammadreza <mr.dorudian@gmail.com>
2022-10-01 22:32:47 +0330

    yara rules added

---
+++ server/yara/yara_interface.py
@@ -0,0 +1,11 @@
+# run yara rules on a file with terminal
+import subprocess
+
+
+def apply_rule(file_path, rules_path):
+    file_name = file_path.split('/')[-1]
+    result = subprocess.run(['yara64', '-r', rules_path, file_path], capture_output=True, text=True)
+    print([result.stdout.replace(file_name, '').strip()])
+
+
+apply_rule('../bad.txt', 'rules/mimikatz.yara')
