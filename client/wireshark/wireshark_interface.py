ea09234a64f43cdebeff11a8050e66154d6dfe0b
Mohammadreza <mr.dorudian@gmail.com>
2022-10-01 22:31:16 +0330

    code refactored

--- client/wireshark/wireshark_interface.py
+++ client/wireshark/wireshark_interface.py
@@ -1,44 +1,124 @@
-import sys
-import time
-import os
-import client.utils as utils
-import capture
+import json
+import multiprocessing
+import pyshark
+from urllib3.connectionpool import xrange
+
+from client.utils import *
+from client import constants
+from multiprocessing import Process, Manager
 
 
-def start():
-    try:
+class Capturer:
+    def __init__(self):
+        self.folder_address = None
+        self.interface = None
+        self.packet_count = None
+        self.load_config()
+        pass
+
+    def __str__(self):
+        return f'Capturer Object\ninterface: {self.interface}, packet_count: {self.packet_count}'
+
+    def load_config(self):
+        self.folder_address = load_config()['address']
+        self.interface = load_config()['interface']
+        self.packet_count = load_config()['packet_count']
+
+    @staticmethod
+    def reformat_json(file_address):
+        with open(file_address, 'r+') as f:
+            all_packets = json.load(f)
+            packet_data = {}
+            for index, packet in enumerate(all_packets):
+                layers = packet['_source']['layers']
+                layer_data = {}
+                for layer in layers:
+                    layer_data[layer] = layers[layer]
+                packet_data[index] = layer_data
+            f.seek(0)
+            f.truncate()
+            json.dump(packet_data, f, indent=4)
+
+    def start(self):
+        file_id = 0
         while True:
-            json_files = [x for x in os.listdir(capture.get_config()['address']) if x.count('.json') > 0]
-            # it will cause a bug without sorting
-            json_files.sort(key=lambda x: int(''.join(filter(str.isdigit, x))))
-            pcap_files = [x for x in os.listdir(capture.get_config()['address']) if x.count('.pcap') > 0]
-            pcap_files.sort(key=lambda x: int(''.join(filter(str.isdigit, x))))
+            try:
+                capture = pyshark.LiveCapture(interface=self.interface,
+                                              output_file=f'{self.folder_address}/wireshark-logs-{file_id}.pcap')
+                capture.sniff(packet_count=self.packet_count)
+                os.system(
+                    f'tshark -r {self.folder_address}/wireshark-logs-{file_id}.pcap -T json > {self.folder_address}/wireshark-logs-{file_id}.json')
+                self.reformat_json(f'{self.folder_address}/wireshark-logs-{file_id}.json')
+                file_id += 1
+                capture.close()
+            except Exception:
+                return
 
-            if len(json_files) > 2 or len(pcap_files) > 2:
-                if len(pcap_files) > 2:
-                    os.remove(f'{capture.get_config()["address"]}/{pcap_files.pop(0)}')
 
-                if len(json_files) > 2:
-                    try:
-                        utils.send_to_server(f'{capture.get_config()["address"]}/{json_files[0]}')
-                        os.remove(f'{capture.get_config()["address"]}/{json_files.pop(0)}')
-                    except ConnectionRefusedError:
-                        print('server not listening')
-            else:
-                time.sleep(10)
-    except Exception as e:
-        print(e)
-        raise KeyboardInterrupt
+class Wireshark:
+    def __init__(self):
+        self.folder_address = None
+        self.load_config()
+        pass
+
+    def __str__(self):
+        return f'Wireshark Object'
+
+    def load_config(self):
+        self.folder_address = load_config()['address']
+
+    def handle_exit(self):
+        send_remaining_files(self.folder_address, 'wireshark')
+        os.chdir(self.folder_address)
+        for file in os.listdir():
+            os.remove(file)
+        return
+
+    def start(self):
+        try:
+            while True:
+                pcap_files = get_file_list(self.folder_address, 'pcap')
+                json_files = get_file_list(self.folder_address, 'json')
+
+                if len(json_files) > constants.SENDING_DELAY or len(pcap_files) > constants.SENDING_DELAY:
+                    if len(pcap_files) > constants.SENDING_DELAY:
+                        os.remove(f'{self.folder_address}/{pcap_files.pop(0)}')
+
+                    if len(json_files) > constants.SENDING_DELAY:
+                        error = send_to_server(f'{self.folder_address}/{json_files[0]}')
+                        if not error:
+                            os.remove(f'{self.folder_address}/{json_files.pop(0)}')
+        except KeyboardInterrupt:
+            self.handle_exit()
+            return
+
+
+def load_config():
+    with open(constants.CLIENT_CONFIG_ADDRESS, 'r') as file:
+        config = yaml.safe_load(file)
+        return {'interface': config['wireshark']['interface'],
+                'packet_count': config['wireshark']['packet_count'],
+                'address': config['wireshark']['address']}
 
 
 if __name__ == '__main__':
+    capturer = Capturer()
+    wireshark = Wireshark()
+
+    processes = []
+
+    manager = Manager()
+
+    p_1 = Process(target=capturer.start)
+    p_1.start()
+    p_2 = Process(target=wireshark.start)
+    p_2.start()
+
     try:
-        capture.start()
-        start()
+        p_1.join()
+        p_2.join()
     except KeyboardInterrupt:
-        time.sleep(1)
-        utils.send_remaining_files(capture.get_config()["address"], 'wireshark')
-        os.chdir(capture.get_config()["address"])
-        for file in os.listdir():
-            os.remove(file)
-        exit(-2)
+        p_1.terminate()
+        p_2.terminate()
+        manager.shutdown()
+        exit(-100)
