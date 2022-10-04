5753f1ead1a6eb490ca82ccc11fb0284f6a76160
Mohammadreza <mr.dorudian@gmail.com>
2022-10-04 21:26:32 +0330

    wireshark clean exit implemented

--- client/wireshark/wireshark_interface.py
+++ client/wireshark/wireshark_interface.py
@@ -1,11 +1,10 @@
 import json
-import multiprocessing
-import pyshark
-from urllib3.connectionpool import xrange
+import subprocess
 
+import pyshark
 from client.utils import *
 from client import constants
-from multiprocessing import Process, Manager
+import threading
 
 
 class Capturer:
@@ -46,8 +45,10 @@ class Capturer:
                 capture = pyshark.LiveCapture(interface=self.interface,
                                               output_file=f'{self.folder_address}/wireshark-logs-{file_id}.pcap')
                 capture.sniff(packet_count=self.packet_count)
-                os.system(
-                    f'tshark -r {self.folder_address}/wireshark-logs-{file_id}.pcap -T json > {self.folder_address}/wireshark-logs-{file_id}.json')
+                subprocess.run(
+                    f'tshark -r {self.folder_address}/wireshark-logs-{file_id}.pcap -T json > '
+                    f'{self.folder_address}/wireshark-logs-{file_id}.json',
+                    shell=True)
                 self.reformat_json(f'{self.folder_address}/wireshark-logs-{file_id}.json')
                 file_id += 1
                 capture.close()
@@ -59,20 +60,12 @@ class Wireshark:
     def __init__(self):
         self.folder_address = None
         self.load_config()
-        pass
 
     def __str__(self):
         return f'Wireshark Object'
 
     def load_config(self):
         self.folder_address = load_config()['address']
-
-    def handle_exit(self):
-        send_remaining_files(self.folder_address, 'wireshark')
-        os.chdir(self.folder_address)
-        for file in os.listdir():
-            os.remove(file)
-        return
 
     def start(self):
         try:
@@ -89,8 +82,15 @@ class Wireshark:
                         if not error:
                             os.remove(f'{self.folder_address}/{json_files.pop(0)}')
         except KeyboardInterrupt:
-            self.handle_exit()
             return
+
+
+def handle_exit(folder_address):
+    send_remaining_files(folder_address, 'wireshark')
+    os.chdir(folder_address)
+    for file in os.listdir():
+        os.remove(file)
+    return
 
 
 def load_config():
@@ -101,24 +101,13 @@ def load_config():
                 'address': config['wireshark']['address']}
 
 
-if __name__ == '__main__':
+def start():
     capturer = Capturer()
     wireshark = Wireshark()
-
-    processes = []
-
-    manager = Manager()
-
-    p_1 = Process(target=capturer.start)
-    p_1.start()
-    p_2 = Process(target=wireshark.start)
-    p_2.start()
-
+    wireshark_thread = threading.Thread(target=wireshark.start)
+    wireshark_thread.daemon = True
     try:
-        p_1.join()
-        p_2.join()
+        wireshark_thread.start()
+        capturer.start()
     except KeyboardInterrupt:
-        p_1.terminate()
-        p_2.terminate()
-        manager.shutdown()
-        exit(-100)
+        handle_exit(wireshark.folder_address)
