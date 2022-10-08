9903c91c7586139c90b8efbde599fc17722e5ad8
Mohammadreza <mr.dorudian@gmail.com>
2022-10-09 00:01:25 +0330

    utility function hash checker added

--- server/utils.py
+++ server/utils.py
@@ -1,13 +1,32 @@
 import os
+import requests
+import server.constants as constants
 
 
 def get_file_list(folder_address):
-    if 'Sysmon' in folder_address:
+    log_type = ''
+    if 'sysmon' in folder_address:
         log_type = 'sysmon'
-    elif 'Wireshark' in folder_address:
+    elif 'wireshark' in folder_address:
         log_type = 'wireshark'
-    else:
-        log_type = 'combined'
+    if 'aggregated' in folder_address:
+        log_type = 'aggregated'
+    if 'filtered' in folder_address:
+        log_type = 'filtered'
     target_file_list = [x for x in os.listdir(folder_address) if x.count(f'{log_type}-logs') > 0]
     target_file_list.sort(key=lambda x: int(''.join(filter(str.isdigit, x))))
     return target_file_list
+
+
+def check_hash(targe_hash):
+    params = {'apikey': constants.API_KEY, 'resource': targe_hash}
+    url = requests.get('https://www.virustotal.com/vtapi/v2/file/report', params=params)
+    json_response = url.json()
+    if json_response['response_code'] == 1:
+        is_threat = json_response['positives'] / json_response['total'] > 0.2
+        if is_threat:
+            return 'True'
+        else:
+            return 'False'
+    else:
+        return 'not found'
