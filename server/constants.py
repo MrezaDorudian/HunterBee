1564ed65ae2a08760a7162b4126bfcc03bfaa5ea
Mohammadreza <mr.dorudian@gmail.com>
2022-10-09 00:00:58 +0330

    some more constants added

--- server/constants.py
+++ server/constants.py
@@ -1,1 +1,5 @@
 SERVER_CONFIG_ADDRESS = r'C:\Users\mrdor\PycharmProjects\Hunterbee\server\config.yaml'
+AGGREGATE_DELAY = 10
+FEATURES = ['ParentProcessGuid', 'Image', 'ParentImage']
+CLUSTER_NUMBER = 2
+API_KEY = '40e1a086cd412e236cf6436a791f26b3bf2c92c5fc3f356db727b6debd7226db'
