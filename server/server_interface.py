d0825992cb6fdbe0869ffb59f9ffa3bbb26520d6
Mohammadreza <mr.dorudian@gmail.com>
2022-11-28 10:38:27 +0330

    server interface updated

--- server/server_interface.py
+++ server/server_interface.py
@@ -1,21 +1,26 @@
-from server.listener import listener
 from processor import aggregate, filter, check
 from listener import listener
 import threading
 if __name__ == '__main__':
     server = listener.Server()
     aggregator = aggregate.LogAggregator()
-    filterer = filter.LogFilterer('sysmon')
-    checker = check.LogChecker('sysmon')
+
+    sysmon_filterer = filter.LogFilterer('sysmon')
+    wireshark_filterer = filter.LogFilterer('wireshark')
+    sysmon_checker = check.LogChecker('sysmon')
+    wireshark_checker = check.LogChecker('wireshark')
     server_thread = threading.Thread(target=server.listen)
     aggregator_thread = threading.Thread(target=aggregator.start)
-    filterer_thread = threading.Thread(target=filterer.start)
-    checker_thread = threading.Thread(target=checker.start)
+    sysmon_filterer_thread = threading.Thread(target=sysmon_filterer.start)
+    wireshark_filterer_thread = threading.Thread(target=wireshark_filterer.start)
+    sysmon_checker_thread = threading.Thread(target=sysmon_checker.start)
+    wireshark_checker_thread = threading.Thread(target=wireshark_checker.start)
     try:
         server_thread.start()
         aggregator_thread.start()
-        filterer_thread.start()
-        checker_thread.start()
+        sysmon_filterer_thread.start()
+        wireshark_filterer_thread.start()
+        sysmon_checker_thread.start()
+        wireshark_checker_thread.start()
     except KeyboardInterrupt:
         exit(0)
-
